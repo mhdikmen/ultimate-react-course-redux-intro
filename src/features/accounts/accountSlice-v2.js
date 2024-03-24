8e5571773b61ec0e7eda998b3197c9c00149757e
Muhammed Hanifi Dikmen <dikmen@live.com>
2024-03-24 17:31:48 +0300

    redux-toolkit added

--- src/features/accounts/accountSlice-v2.js
+++ src/features/accounts/accountSlice-v2.js
@@ -1,70 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
-const accountSlice = createSlice({
-  name: "account",
-  initialState,
-  reducers: {
-    deposit(state, action) {
-      console.log(action);
-      state.balance += action.payload;
-      state.isLoading = false;
-    },
-    withdraw(state, action) {
-      state.balance -= action.payload;
-    },
-    requestLoan: {
-      prepare(amount, purpose) {
-        return {
-          payload: { amount, purpose },
-        };
-      },
-      reducer(state, action) {
-        if (state.loan > 0) return;
-        state.loan = action.payload.amount;
-        state.loanPurpose = action.payload.purpose;
-        state.balance += action.payload.amount;
-      },
-    },
-    payLoan(state) {
-      state.balance -= state.loan;
-      state.loan = 0;
-      state.loanPurpose = "";
-    },
-    convertingCurrency(state) {
-      state.isLoading = true;
-    },
-  },
-});
-
-export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
-
-export function deposit(amount, currency) {
-  if (currency === "USD") return { type: "account/deposit", payload: amount };
-
-  return async function (dispatch, getState) {
-    console.log(getState());
-    dispatch({ type: "account/convertingCurrency" });
-    setTimeout(async () => {
-      const host = "api.frankfurter.app";
-      const res = await fetch(
-        `https://${host}/latest?amount=${amount}&from=${currency}&to=USD`
-      );
-      const data = await res.json();
-      const converted = data.rates.USD;
-      dispatch({ type: "account/deposit", payload: converted });
-    }, 1500);
-  };
-}
-
-export default accountSlice.reducer;
-/*
 export default function accountReducer(state = initialStateAccount, action) {
   console.log("state", state);
   console.log("action", action);
@@ -134,4 +74,3 @@ export function payLoan() {
     type: "account/payLoan",
   };
 }
-*/
